30335f0392a14d9529c4d2fb5363b9e45630717d
ak1n <11824083+ak1n@users.noreply.github.com>
2018-10-25 17:21:14 -0400

    revert for now cnf & redis changes given mysql problems

--- nextcloud-jail.sh
+++ nextcloud-jail.sh
@@ -223,7 +223,7 @@ fi
 # Copy and edit pre-written config files
 iocage exec ${JAIL_NAME} cp -f /mnt/configs/httpd.conf /usr/local/etc/apache24/httpd.conf
 iocage exec ${JAIL_NAME} cp -f /mnt/configs/php.ini /usr/local/etc/php.ini
-#iocage exec ${JAIL_NAME} cp -f /mnt/configs/redis.conf /usr/local/etc/redis.conf
+iocage exec ${JAIL_NAME} cp -f /mnt/configs/redis.conf /usr/local/etc/redis.conf
 iocage exec ${JAIL_NAME} cp -f /mnt/configs/001_mod_php.conf /usr/local/etc/apache24/modules.d/001_mod_php.conf
 if [ $NO_CERT -eq 1 ]; then
   iocage exec ${JAIL_NAME} cp -f /mnt/configs/nextcloud-nossl.conf /usr/local/etc/apache24/Includes/${HOST_NAME}.conf
@@ -233,23 +233,24 @@ fi
 iocage exec ${JAIL_NAME} cp -f /mnt/configs/www.conf /usr/local/etc/php-fpm.d/
 
 #mariadb/mysql handling
-#iocage exec ${JAIL_NAME} cp -f /usr/local/share/mysql/my-small.cnf /var/db/mysql/my.cnf
+iocage exec ${JAIL_NAME} cp -f /usr/local/share/mysql/my-small.cnf /var/db/mysql/my.cnf
 #iocage exec ${JAIL_NAME} "echo '' >> /var/db/mysql/my.cnf"
 #iocage exec ${JAIL_NAME} "echo '[client-server]' >> /var/db/mysql/my.cnf"
 #iocage exec ${JAIL_NAME} "echo '!include /var/db/mysql/db_server_my.cnf' >> /var/db/mysql/my.cnf"
-cp "$CONFIGS_PATH/$DB_CNF.default" $CONFIGS_PATH/$DB_CNF
-sed -i '' "s|\[\[DB_SOCKET\]\]|\/tmp\/mysql.sock|g" $CONFIGS_PATH/$DB_CNF
-sed -i '' "s|\[\[DB_BASEDIR\]\]|\/usr\/local|" $CONFIGS_PATH/$DB_CNF
-sed -i '' "s|\[\[DB_DATADIR\]\]|\/var\/db\/mysql|" $CONFIGS_PATH/$DB_CNF
-sed -i '' "s|\[\[DB_LC_MESSAGES_DIR\]\]|\/usr\/local|" $CONFIGS_PATH/$DB_CNF
-sed -i '' "s|\[\[DB_PID_FILE\]\]|\/var\/db\/mysql\/JAILNAME.pid|" $CONFIGS_PATH/$DB_CNF
-iocage exec ${JAIL_NAME} cp -f /mnt/configs/db_server_my.cnf /var/db/mysql/my.cnf
+
+#cp "$CONFIGS_PATH/$DB_CNF.default" $CONFIGS_PATH/$DB_CNF
+#sed -i '' "s|\[\[DB_SOCKET\]\]|\/tmp\/mysql.sock|g" $CONFIGS_PATH/$DB_CNF
+#sed -i '' "s|\[\[DB_BASEDIR\]\]|\/usr\/local|" $CONFIGS_PATH/$DB_CNF
+#sed -i '' "s|\[\[DB_DATADIR\]\]|\/var\/db\/mysql|" $CONFIGS_PATH/$DB_CNF
+#sed -i '' "s|\[\[DB_LC_MESSAGES_DIR\]\]|\/usr\/local|" $CONFIGS_PATH/$DB_CNF
+#sed -i '' "s|\[\[DB_PID_FILE\]\]|\/var\/db\/mysql\/JAILNAME.pid|" $CONFIGS_PATH/$DB_CNF
+#iocage exec ${JAIL_NAME} cp -f /mnt/configs/db_server_my.cnf /var/db/mysql/my.cnf
 
 iocage exec ${JAIL_NAME} sed -i '' "s/yourhostnamehere/${HOST_NAME}/" /usr/local/etc/apache24/Includes/${HOST_NAME}.conf
 iocage exec ${JAIL_NAME} sed -i '' "s/jailiphere/${JAIL_IP}/" /usr/local/etc/apache24/Includes/${HOST_NAME}.conf
 iocage exec ${JAIL_NAME} sed -i '' "s/yourhostnamehere/${HOST_NAME}/" /usr/local/etc/apache24/httpd.conf
 iocage exec ${JAIL_NAME} sed -i '' "s/#skip-networking/skip-networking/" /var/db/mysql/my.cnf
-iocage exec ${JAIL_NAME} sed -i '' "s/#server-id/server-id/" /var/db/mysql/my.cnf
+#iocage exec ${JAIL_NAME} sed -i '' "s/#server-id/server-id/" /var/db/mysql/my.cnf
 iocage exec ${JAIL_NAME} sed -i '' "s|mytimezone|${TIME_ZONE}|" /usr/local/etc/php.ini
 # iocage exec ${JAIL_NAME} openssl dhparam -out /usr/local/etc/pki/tls/private/dhparams_4096.pem 4096
 iocage restart ${JAIL_NAME}
